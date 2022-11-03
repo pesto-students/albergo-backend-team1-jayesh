4b6967ed6b255c71469e01ee565a3cc203dfef2d
Siddhant-ops <siddhantdalvi3@gmail.com>
2022-11-03 17:18:09 +0530

    hotel model comparison condition changed

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,93 +1,91 @@
 const AppError = require('./../utils/appError');
 const APIFeatures = require('./../utils/apiFeatures');
+const Hotel = require("../models/hotelModel");
 
 exports.getAll = (Model) => async (req, res, next) => {
+  let filter = {};
+  if (req.params.hotelId) filter = { hotel: req.params.hotelId };
 
-    let filter = {};
-    if (req.params.hotelId) filter = { hotel: req.params.hotelId };
+  const features = new APIFeatures(Model.find(filter), req.query)
+    .sort()
+    .paginate();
+  const doc = await features.query;
 
-    const features = new APIFeatures(Model.find(filter), req.query)
-        .sort()
-        .paginate()
-    const doc = await features.query;
-
-    res.status(200).json({
-        status: 'success',
-        result: doc.length,
-        data: {
-            data: doc
-        }
-    });
+  res.status(200).json({
+    status: "success",
+    result: doc.length,
+    data: {
+      data: doc,
+    },
+  });
 };
 
 // Create
 exports.createOne = (Model) => async (req, res, next) => {
-    const doc = await Model.create(req.body);
-    res.status(201).json({
-        status: 'success',
-        data: {
-            data: doc,
-        },
-    });
+  const doc = await Model.create(req.body);
+  res.status(201).json({
+    status: "success",
+    data: {
+      data: doc,
+    },
+  });
 };
 
 // Read
 exports.getOne = (Model) => async (req, res, next) => {
+  let doc;
 
-    let doc;
-    if (Model === 'Hotel')
-        doc = await Model.find({ slug: req.params.slug });
-    else
-        doc = await Model.findById(req.params.id);
-    if (!doc) {
-        return next(new AppError('No document found with that ID', 404, res));
-    }
-    res.status(200).json({
-        status: 'success',
-        data: {
-            data: doc,
-        },
-    });
+  if (Model === Hotel) doc = await Model.find({ slug: req.params.id });
+  else doc = await Model.findById(req.params.id);
+
+  if (!doc) {
+    return next(new AppError("No document found with that ID", 404, res));
+  }
+  res.status(200).json({
+    status: "success",
+    data: doc[0],
+  });
 };
 
 // Update
 exports.updateOne = (Model) => async (req, res, next) => {
+  let doc;
 
-    let doc;
-    if (Model === 'Hotel')
-        doc = await Model.findOneAndUpdate({ slug: req.params.slug }, req.body, {
-            new: true,
-            runValidators: true,
-        });
-    else
-        doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true,
-        });
-    if (!doc) {
-        return next(new AppError('No document found with that ID', 404, res));
-    }
-    res.status(200).json({
-        status: 'success',
-        data: {
-            data: doc,
-        }
+  if (Model === Hotel)
+    doc = await Model.findOneAndUpdate({ slug: req.params.slug }, req.body, {
+      new: true,
+      runValidators: true,
     });
+  else
+    doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+  if (!doc) {
+    return next(new AppError("No document found with that ID", 404, res));
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      data: doc,
+    },
+  });
 };
 
 // Delete
 exports.deleteOne = (Model) => async (req, res, next) => {
+  let doc;
 
-    let doc;
-    if (Model === 'Hotel')
-        doc = await Model.findOneAndDelete({ slug: req.params.slug });
-    else
-        doc = await Model.findByIdAndDelete(req.params.id);
-    if (!doc) {
-        return next(new AppError('No document found with that ID', 404, res));
-    }
-    res.status(204).json({
-        status: 'success',
-        data: null,
-    });
+  if (Model === Hotel)
+    doc = await Model.findOneAndDelete({ slug: req.params.slug });
+  else doc = await Model.findByIdAndDelete(req.params.id);
+
+  if (!doc) {
+    return next(new AppError("No document found with that ID", 404, res));
+  }
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
 };
