06a8f22bbf6147103f857234fda1d6774bf69b1f
ShobhitSahoo <shobhitsahoo46000@gmail.com>
2022-11-05 09:33:31 +0530

    log changes

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -31,36 +31,36 @@ exports.searchByAny = async (req, res, next) => {
 };
 
 exports.advSearch = async (req, res, next) => {
-    const from_date = req.body.from_date;
-    const to_date = req.body.to_date;
-    const bookings = await Booking
-        .find({
-            $or: [
-                { start: { $gte: from_date, $lte: to_date } },
-                {
-                    end: { $gte: from_date, $lte: to_date }
-                },
-                {
-                    $and: [{ start: { $lte: from_date } }, { end: { $gte: to_date } }]
-                },
-            ],
-        })
-        .select('room');
-    const roomIds = bookings.map(b => b.room);
+  const from_date = req.body.from_date;
+  const to_date = req.body.to_date;
+  const bookings = await Booking
+    .find({
+      $or: [
+        { start: { $gte: from_date, $lte: to_date } },
+        {
+          end: { $gte: from_date, $lte: to_date }
+        },
+        {
+          $and: [{ start: { $lte: from_date } }, { end: { $gte: to_date } }]
+        },
+      ],
+    })
+    .select('room');
+  const roomIds = bookings.map(b => b.room);
 
-    const availableRooms = await Room.find({ _id: { $nin: roomIds } })
-    const availableRooms1 = await Room
-        .find({ _id: { $nin: roomIds } })
-        .populate('hotel');
-    res.status(200).json({
-        status: 'success',
-        data: {
-            data: availableRooms
-        }
-    });
+  const availableRooms = await Room.find({ _id: { $nin: roomIds } })
+  const availableRooms1 = await Room
+    .find({ _id: { $nin: roomIds } })
+    .populate('hotel');
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: availableRooms
+    }
+  });
 };
 
-// exports.createHotel = factory.createOne(Hotel);
+exports.createHotel = factory.createOne(Hotel);
 
 exports.getHotel = factory.getOne(Hotel);
 
